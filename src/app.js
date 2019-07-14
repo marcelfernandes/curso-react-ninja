267119d8c85a9e3d1b5913078918fb50dc539574
Marcel Fernandes <marcel.fernandes@gmail.com>
2019-07-14 00:36:13 -0300

    M1#A30

--- src/app.js
+++ src/app.js
@@ -1,16 +1,53 @@
 'use strict'
 
 import React, { Component } from 'react'
-import Button from './button'
 
 class App extends Component {
-
+    constructor () {
+        super()
+        this.state = {
+            value: 'Valor inicial',
+            checked: false,
+            select: '2'
+        }
+    }
     render () {
         return (
             <div>
-                <Button handleClick={() => console.log('click')}>
-                    Click
-                </Button>
+                <form>
+                    <input type='text' value={this.state.value} onChange={(e) => {
+                        console.log(e)
+                        this.setState({
+                            value: e.target.value
+                        })
+                    }} />
+
+                    <label>
+                        <input type='checkbox' value='my-checkbox' 
+                            checked={this.state.checked}
+                            onChange={(e) => {
+                                this.setState({
+                                    checked: e.target.checked
+                                })
+                            }} />
+                        Checkbox
+                    </label>
+
+                    <input type='radio' name='rd' value='1' /> radio 1
+                    <input type='radio' name='rd' value='2' defaultChecked /> radio 2
+
+                    <br /><br /><br />
+
+                    <select multiple value={this.state.select} onChange={(e) => {
+                        this.setState({
+                            select: e.target.select
+                        })
+                    }}>
+                        <option value='1'>Opt 1</option>
+                        <option value='2'>Opt 2</option>
+                        <option value='3'>Opt 3</option>
+                    </select>
+                </form>
             </div>
         )
     }
